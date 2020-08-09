a792a46f78414414da4f279ec04b3a1367a3650c
AkhileshNS <nsakhilesh02@gmail.com>
2020-08-09 13:31:21 +0530

    Initial commit

---
+++ sendgrid-subscribe-button/sendgrid-subscribe-button.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package SSB
+ */
+/*
+Plugin Name: SendGrid Subscribe Button
+Plugin URI: https://github.com/akhileshns/sendgrid-subscribe-button/
+Description: A simple subscribe button for Sendgrid
+Version: 1.0.0
+Author: AkhileshNS
+Author URI: https://github.com/AkhileshNS
+License: MIT
+Text Domain: 
+*/
+?>
